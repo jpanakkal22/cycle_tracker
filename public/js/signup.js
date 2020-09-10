d0fb9a9eb0e795e72ecaec19c9adf2d0fbdd6d7a
New Dad <josh_panakkal3122@hotmail.com>
2020-09-10 16:05:44 -0400

    corrected references to html and userData object

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,43 +3,38 @@ $(document).ready(function() {
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
-  var ageInput = $("input#age-input");
   var firstNameInput = $("input#firstName-input");
-  var lastNameInput = $("input#lastName-input");
-
+ 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
+      firstName: firstNameInput.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim(),
-      age: ageInput.val(),
-      firstName: firstNameInput.val().trim(),
-      lastName: lastNameInput.val().trim()
     };
 
-    if (!userData.email || !userData.password || !userData.firstName || !userData.lastName) {
+    console.log(userData);
+
+    if (!userData.email || !userData.password || !userData.firstName) {
       return;
     }
     // If we have an email and password, run the signUpUser function
     signUpUser(userData);
-    emailInput.val("");
-    passwordInput.val("");
-    ageInput.val("");
     firstNameInput.val("");
-    lastNameInput.val("");
+    emailInput.val("");
+    passwordInput.val("");    
   });
 
   // Does a post to the signup route. If successful, we are redirected to the allexercises page
   // Otherwise we log any errors
   function signUpUser() {
     var userData = {
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim(),
-      age: ageInput.val(),
       firstName: firstNameInput.val().trim(),
-      lastName: lastNameInput.val().trim()
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim()      
     };
+    
     $.post("/api/signup", userData)
       .then(function(data) {
         window.location.replace("/members");
