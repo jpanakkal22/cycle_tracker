818fa2ff6c0b4037993a7ffc4798da1cb6b76b29
New Dad <josh_panakkal3122@hotmail.com>
2020-09-12 16:36:48 -0400

    add template literal for member name

--- public/js/members.js
+++ public/js/members.js
@@ -2,8 +2,7 @@ $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.firstName);
-    console.log(data.firstName);
+    $(".member-name").text(`${data.firstName}${"!"}`);
     $("#id-input").val(data.id);
   });
  
