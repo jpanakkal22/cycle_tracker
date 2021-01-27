8a4a35958a9d7d4a03b95d0d808d8d0f709acb99
New Dad <josh_panakkal3122@hotmail.com>
2021-01-26 20:43:27 -0500

    add 'get' route for login flash messages

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,24 +1,29 @@
 // Requiring our models and passport as we've configured it
-var db = require("../models");
-var passport = require("../config/passport");
+const db = require("../models");
+const passport = require("../config/passport");
 
-module.exports = function(app) {
+module.exports = (app) => {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), (req, res) => {
-    // Sending back a password, even a hashed password, isn't a good idea
+  app.get("/api/login", (req, res) => {
+    const errors = req.flash().error || [];
+    res.send(errors);
+  })
+
+  app.post("/api/login", passport.authenticate("local", { failureFlash: true 
+    }), (req, res) => {
+    // Sending back a password, even a hashed password, isn't a good idea    
     res.json({
       email: req.user.email,
       id: req.user.id
-    });
+    });     
   });
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", (req, res) => {
-    console.log(req.body);
     db.User.create(req.body)
       .then(() => {
         res.redirect(307, "/api/login");
@@ -60,10 +65,9 @@ module.exports = function(app) {
   
   // Route for posting all user challenge data to database
   app.post("/api/user-data", (req, res) => {
-    console.log(req.body);
     // Call database to see if user has a challenge entry
     db.cycleChallenge.findAll({UserId:req.user.id})
-    .then(function(allStats){
+    .then((allStats) => {
      console.log(allStats);
       // res.json(allStats);      
         if(allStats.length > 0){
