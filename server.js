a951fef3d7afcc9b360fdd9e5a24a730ade05900
New Dad <josh_panakkal3122@hotmail.com>
2021-01-26 20:58:17 -0500

    add connect flash package and configure server.js to use flash

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
+const flash = require("connect-flash");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -19,6 +20,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
