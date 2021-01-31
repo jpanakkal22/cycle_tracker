eb8eee0e58fba05418f52e0b365a3f7ab83ca299
New Dad <josh_panakkal3122@hotmail.com>
2021-01-30 20:53:04 -0500

    starting from scratch with post route for data entries

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -20,7 +20,7 @@ module.exports = (app) => {
     });     
   });
 
-   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", (req, res) => {
@@ -46,68 +46,26 @@ module.exports = (app) => {
       res.json({});
     } else {
       // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
+      // Sending back a password, even a hashed password, isn't a good idea      
       res.json({
         email: req.user.email,
         id: req.user.id,
         firstName: req.user.firstName
       });
     }
-  });
-  // Route for setting goal
-  // Route for reading goal
-  // Route for setting distance and time
-  // Route for reading all distance and time data / calculate total distance and time
-  // Route for updating distance or time entry
-  // Route for deleting distance and time entry
-  // Route to send final data when goal is reached
-  // Route to retrieve totals vs. goals
+  }); 
   
   // Route for posting all user challenge data to database
   app.post("/api/user-data", (req, res) => {
-    // Call database to see if user has a challenge entry
-    db.cycleChallenge.findAll({UserId:req.user.id})
-    .then((allStats) => {
-     console.log(allStats);
-      // res.json(allStats);      
-        if(allStats.length > 0){
-          var miles = 0;
-          var duration = 0;
-          for(var i = 0; i < allStats.length; i++){
-            miles += allStats[i].miles;
-            duration += allStats[i].duration
-          }
-
-          miles += JSON.parse(req.body.miles);
-          duration += JSON.parse(req.body.duration);
-
-          db.cycleChallenge.findOne({
-            id: allStats[0].id
-          })
-          console.log(miles);
-          console.log(duration);
-          res.json({miles: miles, duration: duration})
-
-        }
-        else {
-          db.cycleChallenge.create({
-            challenge: req.body.challenge,
-            miles: req.body.miles,
-            duration: req.body.duration,      
-            UserId: req.body.id
-          })
-            .then(function(allStats) {
-              res.json(allStats);      
-            })
-        }
-    });        
-
-    // If so, get the entry, update values
-
-    // Else, create an entry
-
-
-
+    db.cycleChallenge.create({
+      miles: req.body.miles,
+      duration: req.body.duration,      
+      UserId: req.body.id
+    })
+      .then(function(allStats) {
+        console.log("Info Added.")
+        res.json(allStats);      
+      });
   });
 
   
@@ -116,7 +74,7 @@ module.exports = (app) => {
   app.get("/api/all-stats", function(req, res) {
       db.cycleChallenge.findAll({})
       .then(function(allStats){
-       
+        console.log(allStats);
         res.json(allStats);
 
         // Maps all database information
