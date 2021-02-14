d392f076321f591581d256bb4ccf1a85d478c439
New Dad <josh_panakkal3122@hotmail.com>
2021-02-13 22:56:05 -0500

    add click event to table entries and 'PUT' route

--- public/js/members.js
+++ public/js/members.js
@@ -30,7 +30,7 @@ const postStats = () => {
         
     // Send the POST request.
     $.ajax("/api/cycle_data", {
-      type: "POST",
+      method: "POST",
       data: userData
     }).then(() => {
       location.reload();
@@ -45,7 +45,7 @@ const postStats = () => {
 
 const updateStats = () => {
   $.get("/api/all_stats/" + userId, (data) => { 
-        console.log(data);
+        
     // Add information to HTML
     for (let i = 0; i < data.allData.length; i++) {
       const tableRow = $("<tr>");
@@ -56,9 +56,9 @@ const updateStats = () => {
       const tableData2 = $("<td>").text(tableDate);
       const tableData3 = $("<td class='edit-data'>").text(data.allData[i].miles);      
       const tableData4 = $("<td class='edit-data'>").text(data.allData[i].duration);      
-      const editBtn = $("<td>" + "<button type='button' class='btn'>" + "<i class='bi bi-pencil-square'>");
+      const checkBtn = $("<td>" + "<button type='button' class='btn change'>" + "<i class='bi bi-check2-circle'>");
       const deleteBtn = $("<td>" + "<button type='button' class='btn'>" + "<i class='bi bi-x-square'>")
-      tableRow.append(tableData1, tableData2, tableData3, tableData4, editBtn, deleteBtn);
+      tableRow.append(tableData1, tableData2, tableData3, tableData4, checkBtn, deleteBtn);
     }
     // Add total miles and duration to HTML
     $("#miles").text(`${data.totalMiles}${' total miles'}`);
@@ -69,6 +69,32 @@ const updateStats = () => {
 // Add contenteditable attribute to miles and duration table data when Edit button is clicked
 $("#editBtn").click(() => {  
   $(".edit-data").attr("contenteditable", "true");
+});
+
+// Using event delegation to capture click events for dynamically created buttons
+$("#tableBody").click((event) => {  
+  if (event.target.matches("i.bi.bi-check2-circle")) {
+    const par = $(event.target).parentsUntil("tbody");
+    const child = par[2].children;
+    
+    // console.log(child[0].innerHTML, child[2].innerHTML, child[3].innerHTML);
+    const changedData = {
+      UserId: userId,
+      dataId: child[0].innerHTML,
+      miles: child[2].innerHTML,
+      duration: child[3].innerHTML 
+    }   
+
+    // Send the POST request.
+    $.ajax("/api/cycle_data", {
+      method: "PUT",      
+      data: changedData
+    }).then(() => {
+      location.reload();
+      updateStats();             
+    });      
+  }
+  
 })
 
 
